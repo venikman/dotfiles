20e7a7b979f65e729b77b6362bd688772a0d899b
Stanislau <nedbailov375426@gmail.com>
2022-02-11 21:32:30 -0500

    clean up

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-ln -sf $PWD/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
-ln -sf $PWD/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+ln -sf $PWD/.vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+ln -sf $PWD/.vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+ln -sf $PWD/.vscode/extensions.json $HOME/Library/Application\ Support/Code/User/extensions.json
 ln -sf $PWD/init.vim $HOME/.config/nvim/init.vim
 ln -sf $PWD/tmux.conf $HOME/.tmux.conf
-ln -sf $PWD/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
+ln -sf $PWD/kitty.conf $HOME/.config/kitty/kitty.conf
 ln -sf $PWD/zshrc $HOME/.zshrc
