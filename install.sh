f4a47e7fe6fc138f00bd53dd818a75cb754d8cd8
Stanislau Niadbailau <second_life@stanislacstudio.mynetworksettings.com>
2022-12-03 21:07:41 -0500

    simple

--- install.sh
+++ install.sh
@@ -4,9 +4,6 @@ ln -sf $PWD/.vscode/settings.json $HOME/Library/Application\ Support/Code/User/s
 ln -sf $PWD/.vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
 ln -sf $PWD/.vscode/extensions.json $HOME/Library/Application\ Support/Code/User/extensions.json
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -sf $PWD/tmux.conf $HOME/.tmux.conf
-ln -sf $PWD/kitty.conf $HOME/.config/kitty/kitty.conf
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 sh $PWD/mac/mac.sh
